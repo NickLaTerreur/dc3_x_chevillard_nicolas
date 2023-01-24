db59efa54517bd12c2b73e9eadfae2357c4222f5
NickLaTerreur <113183270+NickLaTerreur@users.noreply.github.com>
2023-01-24 11:03:38 -0500

    exo 4

---
+++ ex04:/ft_print_is _negative.js
@@ -0,0 +1,13 @@
+
+function printNegative(n) {
+  if (n <= 0) {
+    console.log("N");
+  } 
+  // if (n === 0) {
+  //   console.log("P");
+  // }
+  else {
+    console.log("P");
+  }
+}
+printNegative(-120);
