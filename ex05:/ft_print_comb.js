3fc0805727639d02b3bc005741262daf428f082c
NickLaTerreur <113183270+NickLaTerreur@users.noreply.github.com>
2023-01-25 07:46:29 -0500

    exo 5 fini

---
+++ ex05:/ft_print_comb.js
@@ -0,0 +1,11 @@
+function printComb() {
+  for (let a = 0; a <= 7; a++) {
+    for (let b = a + 1; b <= 8; b++) {
+      for (let c = b + 1; c <= 9; c++) {
+        console.log(a, b, c);
+      }
+    }
+  }
+}
+
+printComb();
