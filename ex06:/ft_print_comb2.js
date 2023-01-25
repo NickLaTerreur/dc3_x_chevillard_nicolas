54a07ec1c3299b9c5711e134d9873b40457e55b6
NickLaTerreur <113183270+NickLaTerreur@users.noreply.github.com>
2023-01-25 08:59:18 -0500

    exo 6 fini

---
+++ ex06:/ft_print_comb2.js
@@ -0,0 +1,16 @@
+function printComb2() {
+  for (let a = 0; a <= 99; a++) {
+    for (let b = a + 1; b <= 99; b++) {
+      if (a < 10 && b < 10) {
+        console.log("0" + a + " 0" + b + ",");
+      } else if (a < 10 && a >= 10) {
+        console.log("0" + a + " " + b + ",");
+      } else if (a >= 10 && b < 10) {
+        console.log(a + " 0" + b + ",");
+      } else {
+        console.log(a + " " + b + ",");
+      }
+    }
+  }
+}
+printComb2();
